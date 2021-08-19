c7a92338c75a17583dc4fc78115e7c79bf3c65b8
Ivan Elkin <elk.iv@yandex.ru>
2021-08-19 05:50:58 +0300

    Work with main.storyboard

--- TwoViews/ViewController.swift
+++ TwoViews/ViewController.swift
@@ -9,9 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var redLightView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
